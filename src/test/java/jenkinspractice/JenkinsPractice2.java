be73e50b1a16a293bc2d2d06936efe69ade2b458
PAPPU YADAV <PAPPU YADAV@DESKTOP-FMKQHNF>
2022-09-23 16:15:53 +0530

    test3

---
+++ src/test/java/jenkinspractice/JenkinsPractice2.java
@@ -0,0 +1,12 @@
+package jenkinspractice;
+
+import org.testng.annotations.Test;
+
+public class JenkinsPractice2 {
+	@Test
+	public void Test2Test() {
+		
+		System.out.println("Test2 ");
+	}
+
+}
---
+++ src/test/java/jenkinspractice/JenkinsPracticeTest.java
@@ -0,0 +1,12 @@
+package jenkinspractice;
+
+import org.testng.annotations.Test;
+
+public class JenkinsPracticeTest {
+
+	@Test
+	public void Test1Test() {
+		System.out.println("Test 1 ");
+	}
+}
+
