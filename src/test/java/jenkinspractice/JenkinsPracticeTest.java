6462452daeb518f1193e6dd1e47ef29ff9bfc963
PAPPU YADAV <PAPPU YADAV@DESKTOP-FMKQHNF>
2022-09-23 16:49:16 +0530

    pass

--- src/test/java/jenkinspractice/JenkinsPracticeTest.java
+++ src/test/java/jenkinspractice/JenkinsPracticeTest.java
@@ -2,12 +2,20 @@ package jenkinspractice;
 
 import org.testng.annotations.Test;
 
+import io.opentelemetry.exporter.logging.SystemOutLogExporter;
+
 public class JenkinsPracticeTest {
 
 	@Test
 	public void Test1Test() {
 		System.out.println("Test 1 ");
-	//
+	//change done
+		String browser = System.getProperty("br");
+		String url = System.getProperty("ur");
+		System.out.println(browser);
+		System.out.println(url);
+		System.out.println("test case pass Successfull");
+			
 	}
 }
 
